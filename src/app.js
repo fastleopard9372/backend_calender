58b6edc20af330a654c47a2bb563b8196cf405a8
fastleopard9372 <fastleopard9372@gmail.com>
2024-04-11 23:34:40 +0900

    test complete

--- src/app.js
+++ src/app.js
@@ -16,43 +16,42 @@ app.use(helmet());
 
 // Rate limit
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, //1 min
-  max: MAX_API_REQUEST_PER_IP_FOR_MINUTE || 100,
+	windowMs: 1 * 60 * 1000, //1 min
+	max: MAX_API_REQUEST_PER_IP_FOR_MINUTE || 100,
 });
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+// app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.use(cookieParser());
 app.use(morgan("tiny"));
 app.use(limiter);
 
-const PORT = process.env.PORT || "3000";
-
 // API Router
 
 app.get("/", (req, res) => {
-  res.send({ helo: "hello" });
+	res.send({ hello: "hello" });
 });
 
 app.use("/api/v1", router);
 
 app.all("*", (req, res, next) => {
-  const error = new Error(`can't find ${req.originalUrl} on the server`);
-  error.status = "fail";
-  error.statusCode = 404;
-  next(error);
+	const error = new Error(`can't find ${req.originalUrl} on the server`);
+	error.status = "fail";
+	error.statusCode = 404;
+	next(error);
 });
 
 // Error Handle Middleware
 
 app.use((error, req, res, next) => {
-  error.statusCode = error.statusCode || 500;
-  error.status = error.status || "error";
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+	error.statusCode = error.statusCode || 500;
+	error.status = error.status || "error";
+	res.status(error.statusCode).json({
+		status: error.status,
+		message: error.message,
+	});
 });
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const dbConnect = require("./db/conn");
 
+const PORT = process.env.PORT || "3000";
 // Start Server if datbase connected
 async function initializeApp() {
-  try {
-    await dbConnect();
-    app.listen(PORT, () => {
-      console.log("Server Started");
-    });
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		await dbConnect();
+		app.listen(PORT, () => {
+			console.log("Server Started");
+		});
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 // start mongodb and express
@@ -19,14 +20,14 @@ initializeApp();
 
 // Handle MongoDB connection close on application termination
 process.on("SIGINT", async () => {
-  console.log("Received SIGINT. Closing MongoDB connection...");
+	console.log("Received SIGINT. Closing MongoDB connection...");
 
-  try {
-    await mongoose.connection.close();
-    console.log("MongoDB connection closed due to application termination");
-    process.exit(0);
-  } catch (error) {
-    console.error("Error closing MongoDB connection:", error);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connection.close();
+		console.log("MongoDB connection closed due to application termination");
+		process.exit(0);
+	} catch (error) {
+		console.error("Error closing MongoDB connection:", error);
+		process.exit(1);
+	}
 });
